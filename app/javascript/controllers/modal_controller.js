1e76c3558966ed8e0137212f27ff0a7c58a9db92
ihortok <itokaryk@gmail.com>
2022-07-03 13:55:59 +0300

    modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.modal = document.getElementById("modal")
+    this.content = document.getElementById("container")
+  }
+
+  open() {
+    this.content.classList.add("blur-sm")
+    this.modal.classList.remove("hidden")
+  }
+
+  close() {
+    this.content.classList.remove("blur-sm")
+    this.modal.classList.add("hidden")
+  }
+}
